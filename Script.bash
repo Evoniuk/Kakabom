025cbc3ed9f4b507792063a4441a0d97ddc63606
Jack Evoniuk <45554423+Evoniuk@users.noreply.github.com>
2022-11-25 14:36:27 -0800

    Remove style-erasing from color table
    Removed the `0m` part from the colors, as it's unnecessary for
    colors and will erase all styles, which may not be desired.

--- Script.bash
+++ Script.bash
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 
 #colur table
-red="\033[0;31m"
-green="\033[0;32m"
-yellow="\033[0;33m"
-blue="\033[0;34m"
-cyan="\033[0;36m"
+red="\033[31m"
+green="\033[32m"
+yellow="\033[33m"
+blue="\033[34m"
+cyan="\033[36m"
 clear="\033[0m"
 
 #menu loop
